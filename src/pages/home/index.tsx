eac0ce7a89e514b6f6209a275b5c3627d30e80a8
Nathan Ferdinando <nferdica@outlook.com>
2024-08-28 13:12:54 -0300

    feat: Create form input

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,28 @@
+import { useState, FormEvent } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
+    const [input, setInput] = useState('')
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent) {
+      e.preventDefault();
+      if(input === '') return;
+      navigate(`/detail/${input}`)
+    }
+
+    function handleGetMore() {
+
+    }
 
     return (
       <main className={styles.container}>
 
-        <form className={styles.form}>
-          <input type='text' placeholder='Digite o nome do ativo' />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input type='text' placeholder='Digite o nome do ativo' value={input} onChange={(e) => setInput(e.target.value)}/>
           <button type='submit'>
             <BsSearch size={30} color='#FFF' />
           </button>
@@ -59,6 +73,10 @@ export function Home() {
           </tbody>
 
         </table>
+
+        <button className={styles.buttonMore} onClick={handleGetMore}>
+          Carregar mais
+        </button>
 
       </main>
     )
